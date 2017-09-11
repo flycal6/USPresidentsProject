9286a9a84706d843c6e6ef297ee9d5039713d598
Brian Thomas <brianraythomas@gmail.com>
2017-09-11 17:00:19 -0600

    pres choice now a session attr

--- src/com/sd/web/PresServlet.java
+++ src/com/sd/web/PresServlet.java
@@ -6,11 +6,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class PresServlet extends HttpServlet {
 	private PresidentDAO presidentDAO;
-	static int presNum;
-	
+//	private Integer presNum;
+
 	@Override
 	public void init() throws ServletException {
 		presidentDAO = new PresidentFileDAO(getServletContext());
@@ -25,24 +26,38 @@ public class PresServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String next =req.getParameter("next");
+		HttpSession session = req.getSession();
+		Integer presNum = (Integer) session.getAttribute("presNum");
+		if (presNum == null) {
+			presNum = 0;
+//			session.setAttribute("presNum", presNum);
+		}
+
+		String next = req.getParameter("next");
 		String submit = req.getParameter("submit");
+
+		// Integer num = (Integer)session.getAttribute("num");
 		int num = 1;
-		if(req.getParameter("Number") != null) {
+		
+		if (req.getParameter("Number") != null) {
 			num = Integer.parseInt(req.getParameter("Number"));
 		}
-		if(next != null && next.equals("Next")) {
-		if (presNum <= presidentDAO.getPresList().size() - 1) {
-			presNum++;
+
+		if (next != null && next.equals("Next")) {
+			if (presNum <= presidentDAO.getPresList().size() - 1) {
+				presNum++;
+			}
 		}
+
+		if (next != null && next.equals("Previous")) {
+			if (presNum != 0) {
+				presNum--;
+			}
 		}
-		if(next != null && next.equals("Previous"))
-		if (presNum != 0) {
-			presNum--;
-		}
-		if(submit != null && submit.equals("Submit")) {
+		if (submit != null && submit.equals("Submit")) {
 			presNum = num - 1;
 		}
+		session.setAttribute("presNum", presNum);
 		req.setAttribute("currentPres", presidentDAO.getPresList().get(presNum));
 		req.setAttribute("numPres", presidentDAO.getPresList().size());
 		req.getRequestDispatcher("/home.jsp").forward(req, resp);
